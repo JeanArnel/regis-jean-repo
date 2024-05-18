c720ff0dabd4ebfb042add69920ccac90ce046ad
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 13:01:59 -0400

    Create variables.tf

---
+++ development/variables.tf
@@ -0,0 +1,26 @@
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+
+//
+//
+
+variable "prefix" {
+  default = "tfvmex"
+}
+
+/*
+variable "nameofmyazurermrg" {
+  type = string
+  default = "regis-jean-rg-test"
+}
+*/
