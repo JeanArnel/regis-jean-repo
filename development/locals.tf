cc9e41b3b74e34384193f2d69fb0c94103c12544
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 17:00:26 -0400

    Update locals.tf

--- development/locals.tf
+++ development/locals.tf
@@ -39,12 +39,6 @@ locals {
   # Create a map where the key is the VM name, and the value is the VM configuration
   virtual_machines_map = { for vm in local.virtual_machines_config.virtual_machines : vm.name => vm }
 
-
-
-
-
-  virtual_machines_map_location = { for vm in local.virtual_machines_config.virtual_machines : vm.location => vm }
-
   # Common location for all resources
 //  location = "canadacentral"
 
