305056200d2af945f8c85f00c785cc1614945ac8
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 12:13:24 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -16,20 +16,20 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   location            = local.azurerm_resource_group.regis-jean-rg.location
 //  location            = azurerm_resource_group.${var.regis-jean-rg}.location
-  resource_group_name = azurerm_resource_group.regis-jean-rg.name
+  resource_group_name = local.azurerm_resource_group.regis-jean-rg.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.regis-jean-rg.name
+  resource_group_name  = local.azurerm_resource_group.regis-jean-rg.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.regis-jean-rg.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = local.azurerm_resource_group.regis-jean-rg.location
+  resource_group_name = local.azurerm_resource_group.regis-jean-rg.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -40,8 +40,8 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.regis-jean-rg.location
-  resource_group_name   = azurerm_resource_group.regis-jean-rg.name
+  location              = local.azurerm_resource_group.regis-jean-rg.location
+  resource_group_name   = local.azurerm_resource_group.regis-jean-rg.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
