ef015d5a9bda479b8f90ff089310cf3802b0b29d
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 13:46:01 -0400

    Update variables.tf

--- development/variables.tf
+++ development/variables.tf
@@ -33,17 +33,14 @@ variable "nameofmyazurermrg" {
 
 variable "resource_group_name_network_name" {
   type = string
-  default = "regis-jean-rg-test"
 }
 
 variable "resource_group_name_network_location" {
   type = string
-  default = "canadacentral"
 }
 
 variable "resource_group_name_network_address" {
   type = list(string)
-  default = ["10.0.0.0/16"]
 }
 
 //
