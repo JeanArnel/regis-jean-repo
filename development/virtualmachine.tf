afefbe1a424fa0ae3abd5e5d245125d436ba668d
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 13:30:54 -0400

    Update virtualmachine.tf

--- development/virtualmachine.tf
+++ development/virtualmachine.tf
@@ -23,11 +23,32 @@ resource "azurerm_virtual_network" "main" {
 
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.regis-jean-rg.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  name                 = var.azurerm_subnet_name
+  resource_group_name  = var.azurerm_subnet_name_resource_group_name
+  virtual_network_name = var.azurerm_subnet_virtual_network_name
+  address_prefixes     = var.azurerm_subnet_address_prefixes
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
