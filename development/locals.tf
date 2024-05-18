9ec4e46a22dd5351dd74eeb176878c6bbd8eacef
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 15:38:49 -0400

    Update locals.tf

--- development/locals.tf
+++ development/locals.tf
@@ -39,6 +39,9 @@ locals {
     environment = "staging"
   }
 
+  # Assuming 'virtual_machines.yaml' is the file where your list of VMs is defined
+  virtual_machines_config = yamldecode(file("${path.module}/configs/virtual_machines.yaml"))
+
   # Common location for all resources
 //  location = "canadacentral"
 
