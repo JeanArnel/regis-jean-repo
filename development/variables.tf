7f0a7b3d1fde53de7d8e1a57c14890042bdbb44c
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 13:29:30 -0400

    Update variables.tf

--- development/variables.tf
+++ development/variables.tf
@@ -46,3 +46,29 @@ variable "resource_group_name_network_address" {
   default = ["10.0.0.0/16"]
 }
 
+//
+//
+// virtual network subnet internals variables
+//
+//
+
+variable "azurerm_subnet_name" {
+  type = string
+  default = "internal"
+}
+
+variable "azurerm_subnet_name_resource_group_name" {
+  type = string
+  default = "regis-jean-rg-test"
+}
+
+variable "azurerm_subnet_virtual_network_name" {
+  type = string
+  default = "main"
+}
+
+variable "azurerm_subnet_address_prefixes" {
+  type = list(string)
+  default = ["10.0.2.0/24"]
+}
+
