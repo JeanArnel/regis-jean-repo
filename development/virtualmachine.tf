96bb477beb8d842db6c486a5923f54c402363cce
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 16:57:35 -0400

    Update virtualmachine.tf

--- development/virtualmachine.tf
+++ development/virtualmachine.tf
@@ -1,5 +1,6 @@
 resource "azurerm_virtual_network" "main" {
   for_each            = local.virtual_machines_map
+  for_each            = local.virtual_machines_map_location
 //  name                = "${var.prefix}-network"
     name                = each.value.name
   address_space       = var.resource_group_name_network_address
