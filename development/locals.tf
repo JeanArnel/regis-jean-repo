13193322aec75e506c5a0b6205802c46bf0a91cb
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-18 15:47:50 -0400

    Update locals.tf

--- development/locals.tf
+++ development/locals.tf
@@ -34,7 +34,7 @@ locals {
   virtual_machines_config = yamldecode(file("${path.module}/configs/virtual_machine.yaml"))
 
   # This creates a list of virtual machine names by iterating over the decoded YAML content
-  virtual_machine_names = [for vm in local.virtual_machines_config.virtual_machines : vm.name]
+//  virtual_machine_names = [for vm in local.virtual_machines_config.virtual_machines : vm.name]
 
   # Create a map where the key is the VM name, and the value is the VM configuration
   virtual_machines_map = { for vm in local.virtual_machines_config.virtual_machines : vm.name => vm }
